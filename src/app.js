10ede3ddfab7c70da5d78f9be685f25242e4cb6d
unknown <patriciastastny@gmail.com>
2019-12-10 14:22:36 -0800

    Solved issue in app.js of LoadingPAge import typo

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
-import LoadingPage from './components/loadingPage';
+import LoadingPage from './components/LoadingPage';
 
 //ReactDOM.render(<IndecisionApp defOptions={['One option','Two options','Three options']}/>, document.getElementById('app'));
 
